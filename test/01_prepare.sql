eff2853ee76b493291fd2fdea352283dbd96a37c
sapmentors <gregor.wolf@gmail.com>
2016-07-16 11:32:17 +0200

    Add and delete also a user COORGANIZER

--- test/01_prepare.sql
+++ test/01_prepare.sql
@@ -1,5 +1,7 @@
 CREATE USER ORGANIZER PASSWORD Init1234;
+CREATE USER COORGANIZER PASSWORD Init1234;
 CREATE USER PARTICIPANT PASSWORD Init1234;
 
 CALL _SYS_REPO.GRANT_ACTIVATED_ROLE ('com.sap.sapmentors.sitreg.roles::organizer','ORGANIZER');
+CALL _SYS_REPO.GRANT_ACTIVATED_ROLE ('com.sap.sapmentors.sitreg.roles::organizer','COORGANIZER');
 CALL _SYS_REPO.GRANT_ACTIVATED_ROLE ('com.sap.sapmentors.sitreg.roles::participant','PARTICIPANT');

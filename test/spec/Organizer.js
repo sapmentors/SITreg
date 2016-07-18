7de261ad40c771c9933f35e3b87e84dccb8c1598
sapmentors <gregor.wolf@gmail.com>
2016-07-18 22:06:12 +0200

    Test for create event

--- test/spec/Organizer.js
+++ test/spec/Organizer.js
@@ -8,6 +8,29 @@ describe("Login ORGANIZER", function() {
     });
 });
 
+describe("Create event", function() {
+    it("should create event", function() {
+        var xhr = new XMLHttpRequest();
+        var create = {
+            "ID": 1,
+            "Location": "München",
+            "EventDate": "/Date(1475798400000)/",
+            "StartTime": "/Date(1475910000000)/",
+            "EndTime": "/Date(1475942400000)/",
+            "MaxParticipants": 80,
+            "HomepageURL": null
+        };
+        xhr.open("POST", "/com/sap/sapmentors/sitreg/odataorganizer/service.xsodata/Events", false);
+        xhr.setRequestHeader("X-CSRF-Token", csrfToken);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(create));
+        expect(xhr.status).toBe(201);
+        expect(xhr.statusText).toBe("Created");
+        var body = xhr.responseText ? JSON.parse(xhr.responseText) : "";
+    });
+});
+
 describe("Logout ORGANIZER", function() {
     it("should logout ORGANIZER", function() {
         logout(csrfToken);

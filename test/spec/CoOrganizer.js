511fac30b83f37bdeb8c11e68fec6c7703a85005
sapmentors <gregor.wolf@gmail.com>
2016-07-22 00:07:37 +0200

    Missing some files in the last commit

--- test/spec/CoOrganizer.js
+++ test/spec/CoOrganizer.js
@@ -4,6 +4,23 @@ describe("Login COORGANIZER", function() {
     });
 });
 
+describe("Change Homepage URL and read event as COORGANIZER", function() {
+    it("should update the Homepage URL and check the change", function() {
+        var xhr = prepareRequest("PATCH", eventUri);
+        var HomepageURL = "http://www.sitmuc.de/";
+        var change = {
+            "HomepageURL": HomepageURL
+        };
+        xhr.send(JSON.stringify(change));
+        expect(xhr.status).toBe(204);
+        // Check MaxParticipants
+        xhr = prepareRequest("GET", eventUri);
+        xhr.send();
+        var body = xhr.responseText ? JSON.parse(xhr.responseText) : "";
+        expect(body.d.HomepageURL).toBe(HomepageURL);
+    });
+});
+
 describe("Logout COORGANIZER", function() {
     it("should logout COORGANIZER", function() {
         logout(csrfToken);

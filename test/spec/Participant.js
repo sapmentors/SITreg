cebf1dca4c5c1252eec9a904d647a99fb9dbf10d
sapmentors <gregor.wolf@gmail.com>
2016-07-23 23:45:51 +0200

    Test to register for event as participant, adjust view and participant create to reflect restricted authorizations

--- test/spec/Participant.js
+++ test/spec/Participant.js
@@ -13,6 +13,24 @@ describe("Register as Organizer", function() {
     });
 });
 
+describe("Register as Participant", function() {
+    it("should add UserName as an Participant of an Event", function() {
+        var register = {
+			ID: 1,
+			EventID: eventID,
+			FirstName: "John",
+			LastName: "Doe",
+			EMail: "John.Doe@test.com",
+			RSVP: "Y",
+			"History.CreatedBy" : "John"
+        };
+        var xhr = prepareRequest("POST", "/com/sap/sapmentors/sitreg/odataparticipant/service.xsodata/Participant");
+        xhr.send(JSON.stringify(register));
+        expect(xhr.status).toBe(201);
+        expect(xhr.statusText).toBe("Created");
+    });
+});
+
 describe("Logout PARTICIPANT", function() {
     it("should logout PARTICIPANT", function() {
         logout(csrfToken);

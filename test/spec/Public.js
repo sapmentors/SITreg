6f6774bf397af1d29211320f63fbd4b57d9ee25b
sapmentors <gregor.wolf@gmail.com>
2016-07-30 17:13:39 +0200

    Test public OData service

---
+++ test/spec/Public.js
@@ -0,0 +1,39 @@
+/*
+
+   Copyright 2016 SAP Mentors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+var publicEventURI = "";
+
+describe("Read events", function() {
+    it("should return event details", function() {
+        var xhr = prepareRequest("GET", "/com/sap/sapmentors/sitreg/odatapublic/service.xsodata/Events");
+        xhr.send();
+        expect(xhr.status).toBe(200);
+        var body = xhr.responseText ? JSON.parse(xhr.responseText) : "";
+        expect(body.d.results.length).toBe(2);
+        publicEventURI = body.d.results[0].__metadata.uri;
+    });
+});
+
+describe("Read participants of first event", function() {
+    it("should return participant details but without EMail", function() {
+        var xhr = prepareRequest("GET", publicEventURI + "/Participants");
+        xhr.send();
+        expect(xhr.status).toBe(200);
+        var body = xhr.responseText ? JSON.parse(xhr.responseText) : "";
+        expect(body.d.results[0].EMail).toBe(undefined);
+    });
+});

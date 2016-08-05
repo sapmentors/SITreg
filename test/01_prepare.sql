bbef61d7bad7931fc5c19d0754b7ae9d2fddbef2
sapmentors <gregor.wolf@gmail.com>
2016-08-05 17:58:20 +0200

    Try will all files #6

---
+++ test/01_prepare.sql
@@ -0,0 +1,15 @@
+DROP USER ORGANIZER;
+DROP USER COORGANIZER;
+DROP USER PARTICIPANT;
+
+DELETE FROM "com.sap.sapmentors.sitreg.data::SITreg.Event";
+DELETE FROM "com.sap.sapmentors.sitreg.data::SITreg.Participant";
+DELETE FROM "com.sap.sapmentors.sitreg.data::SITreg.CoOrganizer";
+
+CREATE USER ORGANIZER PASSWORD Init1234;
+CREATE USER COORGANIZER PASSWORD Init1234;
+CREATE USER PARTICIPANT PASSWORD Init1234;
+
+CALL _SYS_REPO.GRANT_ACTIVATED_ROLE ('com.sap.sapmentors.sitreg.roles::organizer','ORGANIZER');
+CALL _SYS_REPO.GRANT_ACTIVATED_ROLE ('com.sap.sapmentors.sitreg.roles::organizer','COORGANIZER');
+CALL _SYS_REPO.GRANT_ACTIVATED_ROLE ('com.sap.sapmentors.sitreg.roles::participant','PARTICIPANT');

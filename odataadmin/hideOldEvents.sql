469c294bf4cfc24c10fb35e8dc57913b5d10d052
Gregor Wolf <gregor.wolf@gmail.com>
2019-01-29 14:33:47 +0100

    Do update only events that are visible

--- odataadmin/hideOldEvents.sql
+++ odataadmin/hideOldEvents.sql
@@ -1,6 +1,8 @@
 SELECT * FROM "com.sap.sapmentors.sitreg.data::SITreg.Event" 
   WHERE "EventDate" < CURRENT_DATE
+     AND "Visible" = 'Y'
   ORDER BY "EventDate" DESC;
 UPDATE "com.sap.sapmentors.sitreg.data::SITreg.Event"
    SET "Visible" = 'N'
-   WHERE "EventDate" < CURRENT_DATE;
+   WHERE "EventDate" < CURRENT_DATE
+     AND "Visible" = 'Y';
